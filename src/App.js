3bc9b24563ee95ad475f5a32bfea05e0855d89e0
albertpai <verdant.light@gmail.com>
2018-07-02 13:30:43 +0100

    Replace TV show titles with covers

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-// import A_City_of_Sadness from './A_City_of_Sadness.jpg';
-// import Games_of_Thrones from './Games_of_Thrones.jpg';
-// import House_of_Cards from './House_of_Cards.jpg';
-// import Robin_Hood from './Robin_Hood.jpg';
-// import Sherlock from './Sherlock.jpg';
-// import Breaking_Bad from './Breaking_Bad.jpg';
+import A_City_of_Sadness from './A_City_of_Sadness.jpg';
+import Games_of_Thrones from './Games_of_Thrones.jpg';
+import House_of_Cards from './House_of_Cards.jpg';
+import Robin_Hood from './Robin_Hood.jpg';
+import Sherlock from './Sherlock.jpg';
+import Breaking_Bad from './Breaking_Bad.jpg';
 import './App.css';
 
 class App extends Component {
@@ -20,20 +20,14 @@ class App extends Component {
           These are the posters of my favourite Movies and TV shows!
         </p>
         <div className="poster-container">
-          <div className="poster-title">A City of Sadness</div>
-          <div className="poster-title">Games of Thrones</div>
-          <div className="poster-title">House of Cards</div>
+          <div className="poster-title"><img src={A_City_of_Sadness} alt="A_City_of_Sadness"/></div>
+          <div className="poster-title"><img src={Games_of_Thrones} alt="Games_of_Thrones" /></div>
+          <div className="poster-title"><img src={House_of_Cards} alt="House_of_Cards" /></div>
         </div>
         <div className="poster-container">
-          <div className="poster-title">Robin Hood</div>
-          <div className="poster-title">Sherlock</div>
-          <div className="poster-title">Breaking Bad</div>
-          {/* <img src={A_City_of_Sadness} alt="A_City_of_Sadness" width="100%" />
-          <img src={Games_of_Thrones} alt="Games_of_Thrones" width="100%" />
-          <img src={House_of_Cards} alt="House_of_Cards" width="100%" />
-          <img src={Robin_Hood} alt="Robin_Hood" width="100%" />
-          <img src={Sherlock} alt="Sherlock" width="100%" />
-          <img src={Breaking_Bad} alt="Breaking_Bad" width="100%" /> */}
+          <div className="poster-title"><img src={Robin_Hood} alt="Robin_Hood" /></div>
+          <div className="poster-title"><img src={Sherlock} alt="Sherlock" /></div>
+          <div className="poster-title"><img src={Breaking_Bad} alt="Breaking_Bad" /></div>
         </div>
       </div>
     );
