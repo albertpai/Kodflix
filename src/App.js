c56b4ef03e0e1fd7708f39d7fc9b4497cf0916e4
albertpai <verdant.light@gmail.com>
2018-07-02 12:11:25 +0100

    Add 6 TV show titles

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
 import React, { Component } from 'react';
-import A_city_of_sadness from './A_city_of_sadness.jpg';
+import logo from './logo.svg';
+// import A_City_of_Sadness from './A_City_of_Sadness.jpg';
+// import Games_of_Thrones from './Games_of_Thrones.jpg';
+// import House_of_Cards from './House_of_Cards.jpg';
+// import Robin_Hood from './Robin_Hood.jpg';
+// import Sherlock from './Sherlock.jpg';
+// import Breaking_Bad from './Breaking_Bad.jpg';
 import './App.css';
 
 class App extends Component {
@@ -7,12 +13,28 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={A_city_of_sadness} className="App-logo" alt="logo" />
+          <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React for the first time hellooooo thereee!!</h1>
         </header>
         <p className="App-intro">
-          <img src={A_city_of_sadness} alt="Logo" width="100%" />
+          These are the posters of my favourite Movies and TV shows!
         </p>
+        <div className="poster-container">
+          <div className="poster-title">A City of Sadness</div>
+          <div className="poster-title">Games of Thrones</div>
+          <div className="poster-title">House of Cards</div>
+        </div>
+        <div className="poster-container">
+          <div className="poster-title">Robin Hood</div>
+          <div className="poster-title">Sherlock</div>
+          <div className="poster-title">Breaking Bad</div>
+          {/* <img src={A_City_of_Sadness} alt="A_City_of_Sadness" width="100%" />
+          <img src={Games_of_Thrones} alt="Games_of_Thrones" width="100%" />
+          <img src={House_of_Cards} alt="House_of_Cards" width="100%" />
+          <img src={Robin_Hood} alt="Robin_Hood" width="100%" />
+          <img src={Sherlock} alt="Sherlock" width="100%" />
+          <img src={Breaking_Bad} alt="Breaking_Bad" width="100%" /> */}
+        </div>
       </div>
     );
   }
