04d36f4cf98905480a68cac26e9c10d927ec50f2
albertpai <verdant.light@gmail.com>
2018-07-03 12:38:56 +0100

    Add the TV show title overlay on hover-over #5

--- src/App.js
+++ src/App.js
@@ -20,14 +20,44 @@ class App extends Component {
           These are the posters of my favourite Movies and TV shows!
         </p> */}
         <div className="poster-container">
-          <div className="poster-title"><img src={A_City_of_Sadness} alt="A_City_of_Sadness"/></div>
-          <div className="poster-title"><img src={Games_of_Thrones} alt="Games_of_Thrones" /></div>
-          <div className="poster-title"><img src={House_of_Cards} alt="House_of_Cards" /></div>
+          <div className="poster-image">
+            <img src={A_City_of_Sadness} alt="A_City_of_Sadness"/>
+            <div className="title-overlay">
+              <h1>A City of Sadness Sadness Sadness</h1>
+            </div> 
+          </div>
+          <div className="poster-image">
+            <img src={Games_of_Thrones} alt="Games_of_Thrones" />
+            <div className="title-overlay">
+              <h1>Games of Thrones</h1>
+            </div>             
+            </div>
+          <div className="poster-image">
+            <img src={House_of_Cards} alt="House_of_Cards" />
+            <div className="title-overlay">
+              <h1>House of Cards</h1>
+            </div>
+          </div>
         </div>
         <div className="poster-container">
-          <div className="poster-title"><img src={Robin_Hood} alt="Robin_Hood" /></div>
-          <div className="poster-title"><img src={Sherlock} alt="Sherlock" /></div>
-          <div className="poster-title"><img src={Breaking_Bad} alt="Breaking_Bad" /></div>
+          <div className="poster-image">
+            <img src={Robin_Hood} alt="Robin_Hood" />
+            <div className="title-overlay">
+              <h1>Robin Hood</h1>
+            </div>
+          </div>
+          <div className="poster-image">
+            <img src={Sherlock} alt="Sherlock" />
+            <div className="title-overlay">
+              <h1>Sherlock</h1>
+            </div>
+          </div>
+          <div className="poster-image">
+            <img src={Breaking_Bad} alt="Breaking_Bad" />
+            <div className="title-overlay">
+              <h1>Breaking Bad</h1>
+            </div>
+          </div>
         </div>
       </div>
     );
