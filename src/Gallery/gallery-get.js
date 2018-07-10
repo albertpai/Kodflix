7ba85928ca26ab73fbef659a7d7bcbd4934f50bf
albertpai <verdant.light@gmail.com>
2018-07-10 15:14:48 +0100

    Pull request (#13-2)
    * Change font colour
    
    * Delete a comma, change a function name, change font colour and move image files to a new folder

--- src/Gallery/gallery-get.js
+++ src/Gallery/gallery-get.js
@@ -1,9 +1,9 @@
-import Sadness from './A_City_of_Sadness.jpg';
-import Games from './Games_of_Thrones.jpg';
-import House from './House_of_Cards.jpg';
-import Robin from './Robin_Hood.jpg';
-import Sherlock from './Sherlock.jpg';
-import Breaking from './Breaking_Bad.jpg';
+import Sadness from '../image/A_City_of_Sadness.jpg';
+import Games from '../image/Games_of_Thrones.jpg';
+import House from '../image/House_of_Cards.jpg';
+import Robin from '../image/Robin_Hood.jpg';
+import Sherlock from '../image/Sherlock.jpg';
+import Breaking from '../image/Breaking_Bad.jpg';
 
 const galleryGet = [
     {
