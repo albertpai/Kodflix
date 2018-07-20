25c3b6ce0e08f1add076ca38c0b0b91350ed3c99
albertpai <verdant.light@gmail.com>
2018-07-20 04:04:20 +0100

    Recreate Heroku rep

--- src/backend/app.js
+++ src/backend/app.js
@@ -6,7 +6,7 @@ const port = process.env.PORT || 3001;
 
 app.get('/rest/shows', (req, res) => res.send(shows()))
 
-// Serve any static files
+// Serve any static files   
 app.use(express.static(path.join(__dirname, '../../build')));
 
 // Handle React routing, return all requests to React app
